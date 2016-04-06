9ea5772157d874b8d903db666c0a90b1bdbfc735
Bob Weigel <rweigel@gmu.edu>
2016-04-06 11:02:52 -0400

    Figure improvements

--- Streamlines/overview.m
+++ Streamlines/overview.m
@@ -27,9 +27,9 @@ figure(1);clf;hold on;
     % Add a dot at the vector base and display value.
     for i = 1:10
 	for j = 1:10
-	    text(j-0.5,i-0.2,...
-		 [sprintf('(%.1f,%.1f)',U(i,j),V(i,j))],...
-		 'Color','black','FontSize',8);
+	    th(i,j) = text(j-0.5,i-0.2,...
+			 [sprintf('(%.1f,%.1f)',U(i,j),V(i,j))],...
+			 'Color','black','FontSize',8);
 	    plot(j,i,'k.','MarkerSize',10);
 	end
     end
@@ -88,9 +88,26 @@ for t = 2:Nsteps
     
 end
 
-plot(px(2),py(2),'g.','MarkerSize',20);
-axis([0 2.5 0 2.5]);
-text(px(2)*1.05,py(2),'Position after first step');
+
+% Adjust text position and size for zoom-in figure.
+for i = 1:2
+    for j = 1:2
+	set(th(i,j),'FontSize',16)
+	tp = get(th(i,j),'Position');
+	tp(1) = tp(1) + 0.35;
+	tp(2) = tp(2) + 0.1;
+	set(th(i,j),'Position',tp);
+	plot(i,j,'k.','MarkerSize',30);
+    end
+end
+% Hid other labels
+set(th(1,3),'Visible','Off')
+set(th(2,3),'Visible','Off')
+set(gca,'FontSize',16);
+
+plot(px(2),py(2),'g.','MarkerSize',40);
+axis([0.5 2.5 0.5 2.5]);
+text(px(2)*1.05,py(2),'Position after first step','FontSize',16);
 saveplots('overview_streamline_step_1');
 
 break    
